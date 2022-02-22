e7515519f32b625aed4699a3157a372d0763ab6a
HowToYEET <kkapke19@student.aau.dk>
2022-02-22 15:16:57 +0100

    made graph appear in plots.html

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
-
+from utils import build_url_dict
+import loss_and_acc as graph
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,24 +10,13 @@ def hello_world():
 @app.route("/plots")
 def plots():
     title = 'Plots!'
-    this_could_be_a_png = ['This should be a png','but, it isnt..', 'b/c its a list']
-    search_term = request.args.get('s', type=str)
-    popularity_relation = request.args.get('pr', type=str)
-    n = request.args.get('n', default=20, type=int)
-
-    if search_term:
-        return render_template("plots.html", title=title, png=this_could_be_a_png)
-    else:
-        return """
-        <h1>No input = No output</h1>
-        How to use:
-        <br><code>http://[HOST]>/plots?s=yalla&pr=play.google.com&n=25 </code>
-        <p><br>Search = s
-        <br>Popularity relation = pr
-        <br>Number of x = n
-        </p>
-        <h3>This is an example of how to use flask to pass url parameters.</h3>
-        """
+    #search_term = request.args.get('s', type=str)
+    #popularity_relation = request.args.get('pr', type=str)
+    #n = request.args.get('n', default=20, type=int)
+    png_path = graph.makeGr()
+    
+    return render_template("plots.html", title=title, png=png_path)
+    
 
 
 if __name__ == '__main__':
