25e460261b5d448998cdba615894315ea8278d6d
HowToYEET <kkapke19@student.aau.dk>
2022-03-23 08:45:03 +0100

    yres
    yes

--- forest_deep/deep_f.py
+++ forest_deep/deep_f.py
@@ -1,7 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 from sklearn.metrics import accuracy_score
-    
+import pickle, sys  
 def Run_Forrest_Run():
     root = './test_and_train_sets'
     test_data = pd.read_csv(root + '/out_TEST.tsv', sep='\t', header=None)
@@ -10,14 +10,25 @@ def Run_Forrest_Run():
     x_test = test_data.loc[:, test_data.columns != 0]
     y_train = train_data[0]
     x_train = train_data.loc[:, test_data.columns != 0]
-    print(y_test)
-    print(x_test)
-    clf = RandomForestClassifier(n_estimators=100)
-    clf.fit(x_train, y_train)
-    prediction_of_y = clf.predict(x_test)
-    acc = accuracy_score(y_true=y_test, y_pred=prediction_of_y)
-    print("Precision: " + str(acc))
+    for n in range(1,3):
 
-for i in range(0,10):
+        clf = RandomForestClassifier(n_estimators=20)
+        clf.fit(x_train, y_train)
+        prediction_of_y = clf.predict(x_test)
+        acc = accuracy_score(y_true=y_test, y_pred=prediction_of_y)
+        print(n ," Precision: " + str(acc))
+    
+    save_clf(clf)
+    df = pd.read_pickle('model.pickle')
+    print(df)
 
+
+def save_clf(model):
+    with open("model.pickle", 'wb') as f:
+        pickle.dump(model, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+if "t" in sys.argv:
     Run_Forrest_Run()
+    print("forest COMPLETED")
+    exit()
