9c0fe80637207a3e2683eea992302c49b9371368
HowToYEET <kkapke19@student.aau.dk>
2022-03-28 08:57:20 +0200

    Update deep_f.py

--- forest_deep/deep_f.py
+++ forest_deep/deep_f.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 from sklearn.metrics import accuracy_score
+from sklearn.feature_selection import SelectFromModel
 import pickle, sys  
 def Run_Forrest_Run():
     root = './test_and_train_sets'
@@ -10,17 +11,22 @@ def Run_Forrest_Run():
     x_test = test_data.loc[:, test_data.columns != 0]
     y_train = train_data[0]
     x_train = train_data.loc[:, test_data.columns != 0]
-    for n in range(1,3):
-
+    for n in range(1,12):
+        feat_clf = SelectFromModel(RandomForestClassifier(n_estimators=20))
         clf = RandomForestClassifier(n_estimators=20)
         clf.fit(x_train, y_train)
+        feat_clf.fit(x_train, y_train)
+        features_selected = x_train.columns[(feat_clf.get_support())]
+        print(len(features_selected))
+        print(features_selected)
+        print(clf.estimators_[0])
         prediction_of_y = clf.predict(x_test)
         acc = accuracy_score(y_true=y_test, y_pred=prediction_of_y)
         print(n ," Precision: " + str(acc))
+        
     
-    save_clf(clf)
-    df = pd.read_pickle('model.pickle')
-    print(df)
+    #save_clf(clf)
+    
 
 
 def save_clf(model):
